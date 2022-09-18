dca4a087f60e56fcbb62ac49a71fea6af10b96f0
Rowee Apor <75659500+Rowee13@users.noreply.github.com>
2022-09-18 10:18:54 +0000

    Day 01

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #1. Create a greeting for your program.
-print("Welcome to the Band Name Generator")
+print("Welcome to the Band Name Generator.")
 #2. Ask the user for the city that they grew up in.
 city_name = input("What is the name of the city where you grew up in?\n")
 #3. Ask the user for the name of a pet.
