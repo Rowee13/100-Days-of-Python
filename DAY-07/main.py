cb11e0a47002087906643e4ee4520d269a914388
Rowee Apor <75659500+Rowee13@users.noreply.github.com>
2022-09-25 22:57:24 +0800

    Day-08 Python Course
    Caesar Cipher Encrypt and Decrypt Project

--- DAY-07/main.py
+++ DAY-07/main.py
@@ -3,12 +3,12 @@
 import random
 from hangman_word import word_list
 
-
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 lives = 6
 
 from hangman_art import logo, stages
+
 print(logo)
 
 # for testing
